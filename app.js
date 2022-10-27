70d74994ead6a2694dd05dc56c7269ac10215591
MobilePlusPc <rbrichard1160@gmail.com>
2022-10-27 19:59:33 +0800

    new files

---
+++ app.js
@@ -0,0 +1,11 @@
+function watchAMovie(){
+     console.log("Watch a movie");
+}
+
+function doYoga(){
+     console.log("Do yoga");
+}
+
+function readAGoodBook(){
+     console.log("Read a good book")
+}
